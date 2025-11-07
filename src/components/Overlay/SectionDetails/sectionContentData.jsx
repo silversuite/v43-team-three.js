51fe2ec575d70a09b9d14427af1d33c1a8d81ce0
silversuite <williamjang92@gmail.com>
2025-11-08 00:55:52 +0300

    refresh project

---
+++ src/components/Overlay/SectionDetails/sectionContentData.jsx
@@ -0,0 +1,18 @@
+// Components
+import TeamCarousel from "../TeamCarousel";
+
+// Sections content data
+const sectionsContentData = [
+  {
+    key: "about",
+    title: "About me",
+    content: (
+      <p>
+        Welcome to my 3D portfolio.<br />
+        I’m a passionate 3D, Mobile, and Full-Stack Developer who loves turning complex ideas into smooth, visually stunning, and high-performance digital experiences. Over the years, I’ve built everything from interactive 3D web experiences and cross-platform mobile apps to robust full-stack systems that power real-world businesses.
+      </p>
+    ),
+  }
+];
+
+export default sectionsContentData;
---
+++ src/components/Overlay/TeamCarousel/teamData.js
@@ -0,0 +1,64 @@
+// Team data
+const teamData = [
+  {
+    name: "Danney",
+    image: "/images/danney-min.jpg",
+    links: [
+      { title: "linkedin", link: "https://silver-suite.vercel.app/" },
+      { title: "github", link: "https://silver-suite.vercel.app/" },
+      { title: "twitter", link: "https://silver-suite.vercel.app/" },
+      { title: "portfolio", link: "https://silver-suite.vercel.app/" },
+    ],
+  },
+  {
+    name: "Sean",
+    image: "/images/sean-min.jpg",
+    links: [
+      { title: "linkedin", link: "https://silver-suite.vercel.app/" },
+      { title: "github", link: "https://silver-suite.vercel.app/" },
+      { title: "twitter", link: "https://silver-suite.vercel.app/" },
+      { title: "portfolio", link: "https://silver-suite.vercel.app/" },
+    ],
+  },
+  {
+    name: "Jane",
+    image: "/images/jane-min.jpg",
+    links: [
+      {
+        title: "linkedin",
+        link: "https://www.linkedin.com/in/eugenia-ishunina",
+      },
+      { title: "github", link: "https://github.com/JaneMoroz" },
+      { title: "twitter", link: "https://twitter.com/jane_moroz" },
+      { title: "portfolio", link: "https://jane-moroz-dev.netlify.app/" },
+    ],
+  },
+  {
+    name: "Szymon",
+    image: "/images/szymon-min.jpg",
+    links: [
+      {
+        title: "linkedin",
+        link: "https://github.com/Speckope",
+      },
+      { title: "github", link: "https://github.com/Speckope" },
+      { title: "twitter", link: "https://github.com/Speckope" },
+      { title: "portfolio", link: "https://github.com/Speckope" },
+    ],
+  },
+  {
+    name: "Zoran",
+    image: "/images/zoran-min.jpg",
+    links: [
+      {
+        title: "linkedin",
+        link: "https://github.com/zvitez",
+      },
+      { title: "github", link: "https://github.com/zvitez" },
+      { title: "twitter", link: "https://github.com/zvitez" },
+      { title: "portfolio", link: "https://github.com/zvitez" },
+    ],
+  },
+];
+
+export default teamData;
